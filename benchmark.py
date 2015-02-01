76e8e6d263cddd201856e9c52192654b185ba651
Zac Konopa <konopaz@onid.oregonstate.edu>
2015-01-31 20:55:55 -0800

    Refactoring the benchmark script.
    The benchmark script now generates data in a way that is much more
    conventient for importing in a spreadsheet for graphing data.

--- benchmark.py
+++ benchmark.py
@@ -6,40 +6,36 @@ import sys, os, getopt, random, time
 def printHelp():
   print
   print os.path.basename(sys.argv[0]) + " [--help|-h]",
-  print "[(--algorithm|-a)=(enumeration|betterenum|divide|linear)] 1000"
+  print "[(--algorithm|-a)=(enumeration|betterenum|divide|linear)]"
   print
   print "Enter the name of the script, optional argument to specify the algorithm, "
   print "and the number of random data points to be generated."
   print 
-  print "--algorithm,-a\t\tspecify and algorithm"
-  print "--help,-h\t\tprint this help message"
+  print "--algorithm,-a\tspecify and algorithm"
+  print "--min\t\tminimum int for rand num gen, defaults to -100"
+  print "--max\t\tmaximum int for rand num gen, defaults to 100"
+  print "--steps\t\tdefaults to 1"
+  print "--stepsize\t\tdefaults to 1000"
+  print "--help,-h\tprint this help message"
+
   print
   print "Sample usage..."
-  print "\t" + os.path.basename(sys.argv[0]) + " -a=linear 1000"
+  print "\t" + os.path.basename(sys.argv[0]) + " -a=linear"
   print
 
 def main(argv):
 
   try:
-    opts, args = getopt.getopt(argv, "ha:", ["help", "algorithm=", "min=", "max="])
+    opts, args = getopt.getopt(argv, "ha:", ["help", "algorithm=", "min=", "max=", "steps=", "stepsize="])
   except getopt.GetoptError:
-    printHelp()
-    exit(2)
-
-  if len(args) == 0:
     printHelp()
     exit(2)
 
   selectedAlgorithm = "linear"
   randIntMax = 100
   randIntMin = -100
-
-  try:
-    randIntCnt = int(args[0])
-  except ValueError:
-    print "Couldn't parse ", argv[0], " as an int."
-    printHelp()
-    exit(2)
+  steps = 1
+  stepsize = 1000
 
   for opt in opts:
     if opt[0] == "--algorithm" or opt[0] == "-a":
@@ -53,35 +49,39 @@ def main(argv):
     elif opt[0] == "--max":
       randIntMax = int(opt[1])
 
+    elif opt[0] == "--steps":
+      steps = int(opt[1])
+
+    elif opt[0] == "--stepsize":
+      stepsize = int(opt[1])
+
     elif opt[0] == "--help" or opt[0] == "-h":
       printHelp()
-      exit(2)
+      exit(1)
 
-  print "Run the", selectedAlgorithm, "algorithm with", randIntCnt, "random ints..."
+  for step in range(1, steps + 1):
 
-  randInts = []
-  for i in range(0, randIntCnt):
-    randInts.append(random.randint(randIntMin, randIntMax))
+    randInts = []
+    for i in range(0, step * stepsize):
+      randInts.append(random.randint(randIntMin, randIntMax))
 
-  startTime = time.clock()
+    startTime = time.clock()
 
-  #print randInts
-  if selectedAlgorithm == "enumeration":
-    sumData = enumerationAlgorithm(randInts)
-  elif selectedAlgorithm == "betterenum":
-    sumData = betterEnumerationAlgorithm(randInts)
-  elif selectedAlgorithm == "divide":
-    sumData = divideAndConquerAlgorithm(randInts)
-  elif selectedAlgorithm == "linear":
-    sumData = linearAlgorithm(randInts)
-  else:
-    print "Unsupported algorithm: ", selectedAlgorithm
-    exit(1)
+    if selectedAlgorithm == "enumeration":
+      sumData = enumerationAlgorithm(randInts)
+    elif selectedAlgorithm == "betterenum":
+      sumData = betterEnumerationAlgorithm(randInts)
+    elif selectedAlgorithm == "divide":
+      sumData = divideAndConquerAlgorithm(randInts)
+    elif selectedAlgorithm == "linear":
+      sumData = linearAlgorithm(randInts)
+    else:
+      print "Unsupported algorithm: ", selectedAlgorithm
+      exit(1)
 
-  endTime = time.clock()
+    endTime = time.clock()
 
-  print sumData
-  print "took ", (endTime - startTime), " seconds"
+    print str(step * stepsize) + "," + str(endTime - startTime)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
